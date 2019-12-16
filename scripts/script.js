c724ff0614904e02d67d59ed958986858dccabf0
Nahzilian <50055124+Nahzilian@users.noreply.github.com>
2019-12-15 19:48:46 -0500

    Test

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
 function getContent(id){
     var x = document.getElementById(id);
-    var content = x.textContent;
-    document.getElementById("rdiv").innerHTML = content;
+
+    if(id == "test"){
+        var content = x.textContent;
+        document.getElementById("rdiv").innerHTML = content;
+    }else if(id == "intro"){
+        var content = x.textContent;
+        document.getElementById("rdiv").innerHTML = content;
+    }
+    
 }
 
+//Make function for each part????
