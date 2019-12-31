6cfc0fee409203940d2b30cd56da7bc655a43b7f
Nahzilian <50055124+Nahzilian@users.noreply.github.com>
2019-12-30 19:02:35 -0500

    React app created

---
+++ portfolio/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component,Fragment } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Content from './components/Content';
+
+function App(){
+  return (
+    
+    <Fragment>
+      <Content />
+      
+    </Fragment>
+  );
+}
+
+export default App;
