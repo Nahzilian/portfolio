7174cb44856c76f857ae06425630088f5d03df51
Nahzilian <50055124+Nahzilian@users.noreply.github.com>
2020-01-07 12:40:17 -0500

    Projects rework

--- portfolio/scripts/script.js
+++ portfolio/scripts/script.js
@@ -1,5 +1,4 @@
 
-
 function swap(id){
     var idname = ["home","projects","about","contact"];
     for(var i = 0; i < idname.length; i++){
@@ -14,16 +13,20 @@ function swap(id){
     }
 }
 
+var c1 = new Checkdiv(true)
+var c2 = new Checkdiv(false)
+var c3 = new Checkdiv(false)
+
+
 function openDiv(id){
     var idname = ["c1","c2","c3"];
     for(var i = 0; i < idname.length; i++){
         if(idname[i] == id){
-            document.getElementById(id).style.display = "block";
+                document.getElementById(id).style.display = "block";
         }else{
             document.getElementById(idname[i]).style.display = "none";
         }
     }
-
 }
 
 
