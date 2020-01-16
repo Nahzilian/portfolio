8fff2309d0b3531a584dc0ede29a8f1c399819da
Nahzilian <50055124+Nahzilian@users.noreply.github.com>
2020-01-15 20:24:21 -0500

    Change new theme

--- portfolio/scripts/script.js
+++ portfolio/scripts/script.js
@@ -1,32 +1,20 @@
+function openDiv(evt, nameDiv) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
 
-function swap(id){
-    var idname = ["home","projects","about","contact"];
-    for(var i = 0; i < idname.length; i++){
-        if(idname[i] == id){
-            document.getElementById(id).style.display = "block";
-        }
-        
-        else{
-            document.getElementById(idname[i]).style.display = "none";
-        }
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
 
-    }
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(nameDiv).style.display = "block";
+  evt.currentTarget.className += " active";
 }
-
-var c1 = new Checkdiv(true)
-var c2 = new Checkdiv(false)
-var c3 = new Checkdiv(false)
-
-
-function openDiv(id){
-    var idname = ["c1","c2","c3"];
-    for(var i = 0; i < idname.length; i++){
-        if(idname[i] == id){
-                document.getElementById(id).style.display = "block";
-        }else{
-            document.getElementById(idname[i]).style.display = "none";
-        }
-    }
-}
-
-
