058b090d6bc734ef90fac2ab0c52300da0ab8927
Nahzilian <50055124+Nahzilian@users.noreply.github.com>
2020-01-17 17:30:53 -0500

    added slideshow and change theme for slide

--- portfolio/scripts/script.js
+++ portfolio/scripts/script.js
@@ -18,3 +18,38 @@ function openDiv(evt, nameDiv) {
   document.getElementById(nameDiv).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+
+var txt = "About";
+var i = 0;
+function typeWriter() {
+  if (i < txt.length) {
+      document.getElementById("abt").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, 100);
+    }
+  else{
+    i = 0;
+    setTimeout(reset, 3000);
+    
+}
+
+function reset(){
+  document.getElementById("abt").innerHTML = "";
+  }
+}
+setInterval(typeWriter,6000);
+
+var count = 0;
+var prev = 0;
+function slideshow(){
+  var slides = document.getElementsByClassName("slides");
+      slides[prev].style.display = "none";
+      slides[count].style.display = "block";
+      prev = count;
+      count++;
+      if(prev == 3){count = 0;}
+}
+
+
+setInterval(slideshow,3000);
