53207c16219644d3cef8862506680b20db9183a2
Doug <nahz3112@gmail.com>
2020-11-25 18:09:07 -0500

    Temp

---
+++ portfolio/src/components/Landing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Landing() {
+    return (
+        <div className = "main">
+            <div className = "custom-border">
+                Content
+            </div>
+            {/* Nav bar */}
+            
+            {/* About */}
+            {/* Contact */}
+            {/* Footer */}
+        </div>
+    )
+}
